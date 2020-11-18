7bf467b34d9b7e641211b0640fd7036f32b82d24
cpramshad <cpramshad@gmail.com>
2020-11-18 16:18:47 +0400

    [feature] - Adding props

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <Person />
+        <h1>Hi, I'm a React App. Im {Math.round(Math.random() * 30)} years old</h1>
+        <Person name="Ram" age="36" />
+        <Person name="Max" age="28" >Hi</Person>
       </div>
       // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'))
     );
