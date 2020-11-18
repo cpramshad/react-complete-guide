6b51d590c8cb06ad1771fe0b289f2d5e2be76e85
cpramshad <cpramshad@gmail.com>
2020-11-18 19:18:29 +0400

    [feature] - Adding state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Ram', age: 36},
+      {name: 'Max', age: 28}
+    ]
+  }
+
+  swithNameHandler = () => {
+    console.log('Clicked');
+    this.setState({
+      persons: [
+        {name: 'Ramshad', age: 36},
+        {name: 'Max', age: 38}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App. Im {Math.round(Math.random() * 30)} years old</h1>
-        <Person name="Ram" age="36" />
-        <Person name="Max" age="28" >Hi</Person>
+        <h1>Hi, I'm a React App.</h1>
+        <button onClick={this.swithNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Hi</Person>
       </div>
       // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'))
     );
