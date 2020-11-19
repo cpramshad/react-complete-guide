a817a08256551d979e0b867b47d0560d18fa5e3a
cpramshad <cpramshad@gmail.com>
2020-11-19 13:29:02 +0400

    [feature] - Adding react hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
+// import React, { useState } from 'react';
 import React, { Component } from 'react';
 import './App.css';
+import person from './Person/Person';
 import Person from './Person/Person';
+
+// const App = props => {
+//   const [personsState, setPersonsState] = useState({
+//     persons: [
+//       { name: 'Ram', age: 36 },
+//       { name: 'Max', age: 28 }
+//     ],
+//     otherState: 'Some other value'
+//   });
+
+//   const swithNameHandler = () => {
+//     setPersonsState({
+//       persons: [
+//         {name: 'Ramshad', age: 36},
+//         {name: 'Max', age: 38}
+//       ]
+//     })
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <h1>Hi, I'm a React App.</h1>
+//       <button onClick={swithNameHandler}>Switch name</button>
+//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >Hi</Person>
+//     </div>
+//     // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'))
+//   );
+// }
+
+
 
 class App extends Component {
   state = {
     persons: [
-      {name: 'Ram', age: 36},
-      {name: 'Max', age: 28}
+      { name: 'Ram', age: 36 },
+      { name: 'Max', age: 28 }
     ]
   }
 
@@ -14,8 +48,8 @@ class App extends Component {
     console.log('Clicked');
     this.setState({
       persons: [
-        {name: 'Ramshad', age: 36},
-        {name: 'Max', age: 38}
+        { name: 'Ramshad', age: 36 },
+        { name: 'Max', age: 38 }
       ]
     })
   }
@@ -26,11 +60,15 @@ class App extends Component {
         <h1>Hi, I'm a React App.</h1>
         <button onClick={this.swithNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Hi</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.swithNameHandler}
+        >Hi</Person>
       </div>
       // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'))
     );
-  }  
+  }
 }
 
 export default App;
