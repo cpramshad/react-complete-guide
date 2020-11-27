33d778a41c0f31a565a926e35eccf1af43c9fbc1
cpramshad <cpramshad@gmail.com>
2020-11-27 19:18:35 +0400

    [feature] - Custom HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
     render() {
         console.log('Person.js - rendering...')
         return (
             <Aux>
-                <div className={classes.person}>
                     <p onClick={this.props.click}>I'm {this.props.name} and {this.props.age} years old</p>
                     <p>{this.props.children}</p>
                     <input type="text" onChange={this.props.change} value={this.props.name} ></input>
-                </div>
             </Aux>
         );
     }
 };
 
-export default Person;
+export default withClass(Person, classes.person);
