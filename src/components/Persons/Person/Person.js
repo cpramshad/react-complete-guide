34443f14467bbfbfe4164123a8c4dd8077db4ad7
cpramshad <cpramshad@gmail.com>
2020-11-27 15:19:52 +0400

    [feature] - Aux HOC component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
 
 class Person extends Component {
     render() {
         console.log('Person.js - rendering...')
         return (
-            <div className={classes.person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and {this.props.age} years old</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.change} value={this.props.name} ></input>
-            </div>
+            <Aux>
+                <div className={classes.person}>
+                    <p onClick={this.props.click}>I'm {this.props.name} and {this.props.age} years old</p>
+                    <p>{this.props.children}</p>
+                    <input type="text" onChange={this.props.change} value={this.props.name} ></input>
+                </div>
+            </Aux>
         );
     }
 };
