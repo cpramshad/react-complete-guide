28c1c0e3fe5ddaf11980cc8591a55bf6471e69e5
cpramshad <cpramshad@gmail.com>
2020-11-27 13:46:01 +0400

    [feature] - component optimisation

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -28,10 +28,10 @@ const Cockpit = (props) => {
         btnClass = classes.red;
     }
 
-    if(props.persons.length <= 2) {
+    if(props.personsLength <= 2) {
         assignedClasses.push(classes.red);
     }
-    if(props.persons.length <= 1) {
+    if(props.personsLength <= 1) {
         assignedClasses.push(classes.bold);
     }
 
@@ -46,4 +46,4 @@ const Cockpit = (props) => {
     );
 };
 
-export default Cockpit;
+export default React.memo(Cockpit);
--- src/containers/App.js
+++ src/containers/App.js
@@ -86,7 +86,7 @@ class App extends Component {
         <button onClick={() => {this.setState({showCockpit: false})}}>Remove Cockpit</button>
         {this.state.showCockpit ? (<Cockpit
           showPersons={this.state.showPersons}
-          persons={this.state.persons}
+          personsLength={this.state.persons.length}
           clicked={this.togglePersonsHandler} />) : null }
         {persons}
       </div>
